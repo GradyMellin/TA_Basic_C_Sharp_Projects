65bee6dd391d1e20cd91f463de276bbe5cd77bfc
GradyMellin <mellingrady@gmail.com>
2020-06-30 15:30:05 -0600

    Student Daily Report and Variables and Data Types

---
+++ StudentDailyReport/StudentDailyReport/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace StudentDailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy\n" +
+                "Student Daily Report\n" + 
+                "What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("What Course are you on?");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string s_pageNumber = Console.ReadLine();
+            short pageNumber = Convert.ToInt16(s_pageNumber);
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
+            string s_needHelp = Console.ReadLine();
+            bool needHelp = Convert.ToBoolean(s_needHelp);
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string posExperiences = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string s_studyHours = Console.ReadLine();
+            short studyHours = Convert.ToInt16(s_studyHours);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+        }
+    }
+}
+
---
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("what is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number is : " + favoriteNumber + "\nYour favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+            decimal moneyInBank = 100.5m;
+            double heightInCentimeters = 219.403284398;
+            float secondsLeft = 2.42f;
+            short tempOnMars = -341;
+            int currentAge = 25;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = false;
+            string rainingStatus = Convert.ToString(isRaining);
+            
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
