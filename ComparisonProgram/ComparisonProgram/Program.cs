3bf9dff4e09896a85f943355f8557791c795ec09
GradyMellin <mellingrady@gmail.com>
2020-07-01 14:01:56 -0600

    income comparison program

---
+++ ComparisonProgram/ComparisonProgram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisonProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program\nPerson 1\nHourly rate:");
+            string s_p1_HrRate = Console.ReadLine();
+            decimal p1_HrRate = Convert.ToDecimal(s_p1_HrRate);
+            Console.WriteLine("Hours worked Per Week:");
+            string s_p1_HrsWorked = Console.ReadLine();
+            int p1_HrsWorked = Convert.ToInt32(s_p1_HrsWorked);
+            decimal p1_annualSalary = p1_HrRate * p1_HrsWorked * 52;
+
+            Console.WriteLine("Person 2\nHourly rate:");
+            string s_p2_HrRate = Console.ReadLine();
+            decimal p2_HrRate = Convert.ToDecimal(s_p2_HrRate);
+            Console.WriteLine("Hours worked Per Week:");
+            string s_p2_HrsWorked = Console.ReadLine();
+            int p2_HrsWorked = Convert.ToInt32(s_p2_HrsWorked);
+            decimal p2_annualSalary = p2_HrRate * p2_HrsWorked * 52;
+
+            Console.WriteLine("Annual salary of Person 1:");
+            Console.WriteLine(p1_annualSalary);
+            Console.WriteLine("Annual salary of Person 2:");
+            Console.WriteLine(p2_annualSalary);
+            Console.WriteLine("Does Person 1 make more money that Person 2?");
+            bool salCompare = p1_annualSalary > p2_annualSalary;
+            Console.WriteLine(salCompare);
+            Console.ReadLine();
+
+        }
+    }
+}
--- MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -33,38 +33,38 @@ namespace MathAndComparisonOperators
             //Console.WriteLine(trueOrFalse);
             //Console.ReadLine();
 
-            Console.WriteLine("Please input a number:");
-            string s_inputNum = Console.ReadLine();
-            int inputNum = Convert.ToInt32(s_inputNum);
-            int multipliedBy50 = inputNum * 50;
-            Console.WriteLine(s_inputNum + " * 50 = " + multipliedBy50.ToString());
+            //Console.WriteLine("Please input a number:");
+            //string s_inputNum = Console.ReadLine();
+            //int inputNum = Convert.ToInt32(s_inputNum);
+            //int multipliedBy50 = inputNum * 50;
+            //Console.WriteLine(s_inputNum + " * 50 = " + multipliedBy50.ToString());
 
-            Console.WriteLine("Please input another number: ");
-            string s_inputNum2 = Console.ReadLine();
-            int inputNum2 = Convert.ToInt32(s_inputNum2);
-            int add25 = inputNum2 + 25;
-            Console.WriteLine(s_inputNum2 + " + 25 = " + add25.ToString());
+            //Console.WriteLine("Please input another number: ");
+            //string s_inputNum2 = Console.ReadLine();
+            //int inputNum2 = Convert.ToInt32(s_inputNum2);
+            //int add25 = inputNum2 + 25;
+            //Console.WriteLine(s_inputNum2 + " + 25 = " + add25.ToString());
 
-            Console.WriteLine("Please input another number: ");
-            string s_inputNum3 = Console.ReadLine();
-            float inputNum3 = Convert.ToSingle(s_inputNum3);
-            float divideBy12point5 = inputNum3 / 12.5f;
-            Console.WriteLine(s_inputNum3 + " / 12.5 = " + divideBy12point5.ToString());
-
-
-            Console.WriteLine("Please input another number: ");
-            string s_inputNum4 = Console.ReadLine();
-            int inputNum4 = Convert.ToInt32(s_inputNum4);
-            bool greaterThan50 = inputNum4 > 50;
-            Console.WriteLine("The expression: " + s_inputNum4 + " > 50 is " + greaterThan50.ToString());
+            //Console.WriteLine("Please input another number: ");
+            //string s_inputNum3 = Console.ReadLine();
+            //float inputNum3 = Convert.ToSingle(s_inputNum3);
+            //float divideBy12point5 = inputNum3 / 12.5f;
+            //Console.WriteLine(s_inputNum3 + " / 12.5 = " + divideBy12point5.ToString());
 
 
-            Console.WriteLine("Please input another number: ");
-            string s_inputNum5 = Console.ReadLine();
-            int inputNum5 = Convert.ToInt32(s_inputNum5);
-            int remainderOver7 = inputNum5 % 7;
-            Console.WriteLine(s_inputNum5 + " % 7 = " + remainderOver7.ToString());
-            Console.ReadLine();
+            //Console.WriteLine("Please input another number: ");
+            //string s_inputNum4 = Console.ReadLine();
+            //int inputNum4 = Convert.ToInt32(s_inputNum4);
+            //bool greaterThan50 = inputNum4 > 50;
+            //Console.WriteLine("The expression: " + s_inputNum4 + " > 50 is " + greaterThan50.ToString());
+
+
+            //Console.WriteLine("Please input another number: ");
+            //string s_inputNum5 = Console.ReadLine();
+            //int inputNum5 = Convert.ToInt32(s_inputNum5);
+            //int remainderOver7 = inputNum5 % 7;
+            //Console.WriteLine(s_inputNum5 + " % 7 = " + remainderOver7.ToString());
+            //Console.ReadLine();
 
 
         }
