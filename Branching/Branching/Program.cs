3bdd3c503b88287c7f15cfe53de18e2065590dab
GradyMellin <mellingrady@gmail.com>
2020-07-02 15:27:18 -0600

    Branching

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int currentTemperature = 70;
+            int roomTemperature = 70;
+
+            Console.WriteLine("Hi Whats your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi, " + name + ", what is the temperature where you are? ");
+            int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            if (currentTemperature == roomTemperature)
+            {
+                Console.WriteLine("It is exactly room temperature.");
+            }
+            else if (currentTemperature > roomTemperature)
+            {
+                Console.WriteLine("It is warmer than room temperature.");
+            }
+            else if (currentTemperature < roomTemperature)
+            {
+                Console.WriteLine("It is cooler than room temperature.");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong here....");
+            }
+            Console.WriteLine("What temperature do you like your thermostat?");
+            int favTemperature = Convert.ToInt32(Console.ReadLine());
+            string favResult = favTemperature == 69 ? "Yes, that is the perfect temperature!" : "Oof, you are absolutely wrong!";
+            Console.WriteLine(favResult);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxWeight = 50;
+
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.\nPlease enter the package weight: ");
+            int packWeight = Convert.ToInt32(Console.ReadLine());
+            if (packWeight <= maxWeight)
+            {
+                Console.WriteLine("Please enter the package width: ");
+                int packWidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package height: ");
+                int packHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length: ");
+                int packLength = Convert.ToInt32(Console.ReadLine());
+                double packageCost = (packWeight * packHeight * packLength * packWidth) / 100.0;
+                Console.WriteLine("The estimated cost of your package is: $"+ packageCost.ToString());
+                Console.WriteLine("Thank you.");
+                
+            }
+            else
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
