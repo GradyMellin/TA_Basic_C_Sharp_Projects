63ada8e60aec1c4ebc0c3b49098eb10dd8a87f85
GradyMellin <mellingrady@gmail.com>
2020-07-03 12:08:23 -0600

    List and Array assignment

---
+++ ArrayAssignment/ArrayAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+class Program
+    {
+        static void Main()
+        {
+            string[] stringArray = { "this", "is", "a", "string", "array", "with", "8", "elements"};
+            Console.WriteLine("Select the index of the string array between 0 and 7: ");
+            int stringIndex = Convert.ToInt32(Console.ReadLine());
+            if (stringIndex < 8 && stringIndex >= 0)
+            {
+                Console.WriteLine(stringArray[stringIndex]);
+            }
+            else
+            {
+                Console.WriteLine("Sorry there is no element at the index you have selected.");
+            }
+
+            int[] intArray = { 1,2,3,4,5,6,7,8,9,10};
+            Console.WriteLine("Select the index of the int array: ");
+            int intIndex = Convert.ToInt32(Console.ReadLine());
+            if (intIndex < 10 && intIndex >= 0)
+            {
+                Console.WriteLine(intArray[intIndex]);
+            }
+            else
+            {
+                Console.WriteLine("Sorry there is no element at the index you have selected.");
+            }
+        
+        
+
+            List<string> stringList = new List<string>() { "this", "is", "a", "string", "list", "with", "8", "elements" };
+            Console.WriteLine("Select the index of the string list: ");
+            int listIndex = Convert.ToInt32(Console.ReadLine());
+            if (listIndex < 8 && listIndex >= 0)
+            {
+                Console.WriteLine(stringList[listIndex]);
+            }
+            else
+            {
+                Console.WriteLine("Sorry there is no element at the index you have selected.");
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 10;
+        //numArray[2] = 15;
+        //numArray[3] = 20;
+        //numArray[4] = 25;
+
+        //int[] numArray2 = new int[] { 1, 2, 3, 4, 5, 6 };
+        //int[] numArray3 = { 1, 2, 3, 4, 5, 6 };
+        //Console.WriteLine(numArray[4]);
+        //Console.WriteLine(numArray2[3]);
+
+        List<int> intList = new List<int>();
+        intList.Add(4);
+        intList.Add(2);
+        intList.Add(9);
+        Console.WriteLine(intList[2]);
+       
+
+        Console.ReadLine();
+    }
+    }
+
