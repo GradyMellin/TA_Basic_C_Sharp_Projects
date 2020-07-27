bde94923fed582000d9f77ec57ab8513dd097f7a
GradyMellin <mellingrady@gmail.com>
2020-07-27 17:52:28 -0600

    start of blackjack app and class and method assignment

---
+++ ClassAssignment/ClassAssignment/InputNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    public class InputNumber
+    {
+        public int TimesTen()
+        {
+            int product = this.NumIn * 10;
+            return product;
+        }
+
+        public int PlusFive()
+        {
+            int sum = this.NumIn + 5;
+            return sum;
+        }
+
+        public int ModulesTwo()
+        {
+            int remainder = this.NumIn % 2;
+            return remainder;
+        }
+
+        public int NumIn { get; set; }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputNumber number = new InputNumber();
+            Console.WriteLine("Please enter a number: ");
+            number.NumIn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You have entered " + number.NumIn);
+            Console.WriteLine(number.NumIn + " * 10 = " + number.TimesTen());
+            Console.WriteLine(number.NumIn + " + 5 = " + number.PlusFive());
+            Console.WriteLine(number.NumIn + " / 2 has a remainder of " + number.ModulesTwo());
+
+            Console.ReadLine();
+        }
+    }
+}
