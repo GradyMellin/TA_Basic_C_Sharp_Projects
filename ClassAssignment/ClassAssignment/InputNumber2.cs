dfe160f41f701f892a5feaa0781769d8bca7982b
GradyMellin <mellingrady@gmail.com>
2020-07-28 12:11:53 -0600

    second class method assignment

---
+++ ClassAssignment/ClassAssignment/InputNumber2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class InputNumber2
+    {
+        public int TimesFive(int num)
+        {
+            int product = num * 5;
+            return product;
+        }
+
+        public int TimesFive(decimal num)
+        {
+            decimal productD = num * 5;
+            int product = Convert.ToInt32(productD);
+            return product;
+        }
+
+        public int TimesFive(string numS)
+        {
+            
+            try
+            {
+                int num = Convert.ToInt32(numS);
+                int product = num * 5;
+                return product;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+            
+        }
+
+        public int Number { get; set; }
+    }
+}
--- ClassAssignment/ClassAssignment/Program.cs
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -10,13 +10,24 @@ namespace ClassAssignment
     {
         static void Main(string[] args)
         {
-            InputNumber number = new InputNumber();
-            Console.WriteLine("Please enter a number: ");
-            number.NumIn = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("You have entered " + number.NumIn);
-            Console.WriteLine(number.NumIn + " * 10 = " + number.TimesTen());
-            Console.WriteLine(number.NumIn + " + 5 = " + number.PlusFive());
-            Console.WriteLine(number.NumIn + " / 2 has a remainder of " + number.ModulesTwo());
+            //InputNumber number = new InputNumber();
+            //Console.WriteLine("Please enter a number: ");
+            //number.NumIn = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("You have entered " + number.NumIn);
+            //Console.WriteLine(number.NumIn + " * 10 = " + number.TimesTen());
+            //Console.WriteLine(number.NumIn + " + 5 = " + number.PlusFive());
+            //Console.WriteLine(number.NumIn + " / 2 has a remainder of " + number.ModulesTwo());
+
+            InputNumber2 number = new InputNumber2();
+            Console.WriteLine("Please enter an integer number: ");
+            int intIn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(intIn + " * 10 = " + number.TimesFive(intIn));
+            Console.WriteLine("Please enter a decimal number: ");
+            decimal decIn = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(decIn + " * 10 = " + number.TimesFive(decIn));
+            Console.WriteLine("Please enter a number as a string: ");
+            string strIn = Console.ReadLine();
+            Console.WriteLine(strIn + " * 10 = " + number.TimesFive(strIn));
 
             Console.ReadLine();
         }
