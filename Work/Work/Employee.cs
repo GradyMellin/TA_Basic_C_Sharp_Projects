51e04c67325a96d1f4ea475009a0cb90a1a7480d
GradyMellin <mellingrady@gmail.com>
2020-07-29 14:11:18 -0600

    Interface assignment.

---
+++ Work/Work/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+        public bool isActivelyEmployed { get; set; }
+        
+
+        public void Quit()
+        {
+            Console.WriteLine("I Quit!");
+            this.isActivelyEmployed = false;
+        }
+    }
+
+}
---
+++ Work/Work/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sample = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+                isActivelyEmployed = true
+            };
+
+            sample.SayName();
+            sample.Quit();
+
+            IQuittable sample2 = new Employee();
+
+            Console.ReadLine();
+        }
+    }
+}
