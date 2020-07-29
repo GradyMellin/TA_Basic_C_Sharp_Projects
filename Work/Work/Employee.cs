0d3cf5412b011c5095cc6a596cb8ac045d92616c
GradyMellin <mellingrady@gmail.com>
2020-07-29 16:21:37 -0600

    overloaded the == operator

--- Work/Work/Employee.cs
+++ Work/Work/Employee.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -12,6 +13,16 @@ namespace Work
         public int Id { get; set; }
         public bool isActivelyEmployed { get; set; }
         
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            bool compare = employee1.Id == employee2.Id;
+            return compare;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            bool compare = employee1.Id == employee2.Id;
+            return compare;
+        }
 
         public void Quit()
         {
--- Work/Work/Program.cs
+++ Work/Work/Program.cs
@@ -14,11 +14,20 @@ namespace Work
             {
                 FirstName = "Sample",
                 LastName = "Student",
+                Id = 1,
+                isActivelyEmployed = true
+            };
+            Employee grady = new Employee()
+            {
+                FirstName = "Grady",
+                LastName = "Mellin",
+                Id = 1,
                 isActivelyEmployed = true
             };
 
             sample.SayName();
             sample.Quit();
+            Console.WriteLine(grady == sample);
 
             IQuittable sample2 = new Employee();
 
