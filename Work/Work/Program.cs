23a42f9ecc1299f9bad2b8a80200b80f4d770811
GradyMellin <mellingrady@gmail.com>
2020-07-30 18:28:18 -0600

    lambda expression lists

--- Work/Work/Program.cs
+++ Work/Work/Program.cs
@@ -14,18 +14,80 @@ namespace Work
             {
                 FirstName = "Sample",
                 LastName = "Student",
-                Id = 1,
-                isActivelyEmployed = true
+                Id = 1
             };
             sample.Things = new List<string>() { "jar", "cup", "keys" };
-            Employee<int> grady = new Employee<int>()
+            Employee<string> grady = new Employee<string>()
             {
                 FirstName = "Grady",
                 LastName = "Mellin",
-                Id = 1,
-                isActivelyEmployed = true
+                Id = 2
             };
-            grady.Things = new List<int>() { 1, 2, 3 };
+            Employee<string> employee3 = new Employee<string>()
+            {
+                FirstName = "Bill",
+                LastName = "Wilson",
+                Id = 3
+            };
+            Employee<string> employee4 = new Employee<string>()
+            {
+                FirstName = "Bob",
+                LastName = "Smith",
+                Id = 4
+            };
+            Employee<string> employee5 = new Employee<string>()
+            {
+                FirstName = "Joe",
+                LastName = "Shmo",
+                Id = 5
+            };
+            Employee<string> employee6 = new Employee<string>()
+            {
+                FirstName = "Jerry",
+                LastName = "Cherry",
+                Id = 6
+            };
+            Employee<string> employee7 = new Employee<string>()
+            {
+                FirstName = "Joe",
+                LastName = "Dirt",
+                Id = 7
+            };
+            Employee<string> employee8 = new Employee<string>()
+            {
+                FirstName = "Donkey",
+                LastName = "Kong",
+                Id = 8
+            };
+            Employee<string> employee9 = new Employee<string>()
+            {
+                FirstName = "Kelly",
+                LastName = "Smelly",
+                Id = 9
+            };
+            Employee<string> employee10 = new Employee<string>()
+            {
+                FirstName = "Minty", 
+                LastName = "Fresh",
+                Id = 10
+            };
+
+            List < Employee<string>> employees = new List<Employee<string>>() { sample, grady, employee3, employee4, employee5, employee6, employee7, employee8, employee9, employee10 };
+            List<Employee<string>> joes = new List<Employee<string>>();
+            foreach (Employee<string> employee in employees)
+            {
+                if (employee.FirstName == "Joe")
+                {
+                    joes.Add(employee);
+                }
+            }
+
+            List<Employee<string>> joes2 = employees.Where(x => x.FirstName == "Joe").ToList();
+
+            List<Employee<string>> id5 = employees.Where(x => x.Id > 5).ToList();
+
+
+
             //sample.SayName();
             //sample.Quit();
             //Console.WriteLine(grady == sample);
@@ -38,20 +100,21 @@ namespace Work
             //    Console.WriteLine(thing);
             //}
             //IQuittable sample2 = new Employee<string>();
-            DayOfTheWeek day = new DayOfTheWeek();
-            Console.WriteLine("Please Enter the current day of the week starting with a capital letter: ");
-            try
-            {
-                string day1 = Console.ReadLine();
-                day.Day = (Days)Enum.Parse(typeof(Days), day1);
-                Console.WriteLine("Today is a " + day.Day);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine("Please enter an actual day of the week.");
-            }
-            
+
+            //DayOfTheWeek day = new DayOfTheWeek();
+            //Console.WriteLine("Please Enter the current day of the week starting with a capital letter: ");
+            //try
+            //{
+            //    string day1 = Console.ReadLine();
+            //    day.Day = (Days)Enum.Parse(typeof(Days), day1);
+            //    Console.WriteLine("Today is a " + day.Day);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine("Please enter an actual day of the week.");
+            //}
+
             Console.ReadLine();
 
         }
