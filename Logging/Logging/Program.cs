24c6f646245cffb32f788ca1296ab9267b57d15b
GradyMellin <mellingrady@gmail.com>
2020-08-03 17:20:07 -0600

    logging

---
+++ Logging/Logging/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Grady Mellin\Desktop\C#_course\TA_Basic_C#_Projects\log.txt", true))
+            {
+                file.WriteLine(number);
+            }
+            Console.WriteLine(number);
+            Console.ReadLine();
+        }
+    }
+}
