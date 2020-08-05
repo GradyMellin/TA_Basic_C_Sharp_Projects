d0b5232e67864adaa9b34d360a0bec7fe92e7717
GradyMellin <mellingrady@gmail.com>
2020-08-05 13:10:34 -0600

    Drill from page 230

---
+++ Drill230_var_const_chain/Drill230_var_const_chain/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill230_var_const
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string constant = "This is a constant";
+            var number = 5;
+            Person test = new Person();
+            Console.WriteLine(test.Name);
+            Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        public Person() : this("Joe Dirt") { }
+        public Person(string name) { Name = name; }
+        public string Name { get; set; }
+    }
+}
